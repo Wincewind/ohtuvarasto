36c4c7a880d07c3ea46465a5f46a180f7d1b46d2
Wincewind <86107954+Wincewind@users.noreply.github.com>
2023-10-31 10:35:53 +0200

    korjattu testi

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -66,4 +66,4 @@ class TestVarasto(unittest.TestCase):
         self.assertEqual(self.varasto.saldo, 0)
 
     def test_str(self):
-        self.assertEqual(str(self.varasto), 'saldo = ~, vielä tilaa 10')
+        self.assertEqual(str(self.varasto), 'saldo = 0, vielä tilaa 10')
